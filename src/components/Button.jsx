0e36ea53e37d2dcc6d5c9d8bf0ef7dd7e2934503
Naman Nigam <namannigam30@gmail.com>
2024-11-07 23:15:31 +0530

    signup page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,7 +10,7 @@ function Button({
 }) 
 {
   return (
-    <button className={`px-4 py-2 rounded-lg mt-5 ${bgColor} ${textColor} ${className}`} {...props}>
+    <button className={`px-4 py-2 rounded-md mt-5 ${bgColor} ${textColor} ${className}`} {...props}>
         {children}
     </button>
     )
